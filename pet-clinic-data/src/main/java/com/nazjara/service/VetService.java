d72a95a2d1d49b6812bec22f1cd8883140247131
Nazar Lipkovskyy <nazar.lipkovskyy@gmail.com>
2019-07-02 22:13:06 +0300

    Add service interfaces, BaseEntity

---
+++ pet-clinic-data/src/main/java/com/nazjara/service/VetService.java
@@ -0,0 +1,14 @@
+package com.nazjara.service;
+
+import com.nazjara.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
