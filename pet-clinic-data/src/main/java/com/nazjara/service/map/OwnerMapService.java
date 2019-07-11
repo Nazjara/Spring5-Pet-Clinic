51d04c5b8f827a81e879c843168f22620d1c59c5
Nazar Lipkovskyy <nazar.lipkovskyy@gmail.com>
2019-07-12 00:09:50 +0300

    Add Pets and Contact info to owners in init data

--- pet-clinic-data/src/main/java/com/nazjara/service/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/nazjara/service/map/OwnerMapService.java
@@ -1,13 +1,26 @@
 package com.nazjara.service.map;
 
 import com.nazjara.model.Owner;
+import com.nazjara.model.Pet;
 import com.nazjara.service.OwnerService;
+import com.nazjara.service.PetService;
+import com.nazjara.service.PetTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    @Autowired
+    public OwnerMapService(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -26,6 +39,22 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
+        if (object.getPets() != null) {
+            object.getPets().forEach(pet -> {
+                if (pet.getPetType() != null) {
+                    if (pet.getPetType().getId() == null) {
+                        pet.setPetType(petTypeService.save(pet.getPetType()));
+                    }
+                } else {
+                    throw new RuntimeException("PetType is required");
+                }
+
+                if (pet.getId() == null) {
+                    Pet savedPet = petService.save(pet);
+                    pet.setId(savedPet.getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
--- pet-clinic-web/src/main/java/com/nazjara/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/nazjara/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.nazjara.bootstrap;
 
 import com.nazjara.model.Owner;
+import com.nazjara.model.Pet;
 import com.nazjara.model.PetType;
 import com.nazjara.model.Vet;
 import com.nazjara.service.OwnerService;
@@ -9,6 +10,8 @@ import com.nazjara.service.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -32,16 +35,38 @@ public class DataLoader implements CommandLineRunner {
         PetType cat = new PetType();
         cat.setName("Cat");
 
-        petTypeService.save(dog);
-        petTypeService.save(cat);
+        dog = petTypeService.save(dog);
+        cat = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Owner's 1 firstname");
         owner1.setLastName("Owner's 1 lastname");
+        owner1.setAddress("Address 1");
+        owner1.setCity("City 1");
+        owner1.setTelephone("Phone 1");
+
+        Pet pet1 = new Pet();
+        pet1.setPetType(dog);
+        pet1.setOwner(owner1);
+        pet1.setBirthDate(LocalDate.now());
+        pet1.setName("Dog");
+
+        owner1.getPets().add(pet1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Owner's 2 firstname");
         owner2.setLastName("Owner's 2 lastname");
+        owner1.setAddress("Address 2");
+        owner1.setCity("City 2");
+        owner1.setTelephone("Phone 2");
+
+        Pet pet2 = new Pet();
+        pet2.setPetType(cat);
+        pet2.setOwner(owner2);
+        pet2.setBirthDate(LocalDate.now());
+        pet2.setName("Cat");
+
+        owner2.getPets().add(pet2);
 
         ownerService.save(owner1);
         ownerService.save(owner2);
