385340a8fb699056432613709374a58ef451c130
Nazar Lipkovskyy <nazar.lipkovskyy@wds.co>
2019-07-17 12:41:58 +0300

    Add JPA services

---
+++ pet-clinic-data/src/main/java/com/nazjara/service/jpa/OwnerJpaService.java
@@ -0,0 +1,56 @@
+package com.nazjara.service.jpa;
+
+import com.nazjara.model.Owner;
+import com.nazjara.repository.OwnerRepository;
+import com.nazjara.service.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class OwnerJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+
+    @Autowired
+    public OwnerJpaService(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepository.deleteById(aLong);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/nazjara/service/jpa/SpecialityJpaService.java
@@ -0,0 +1,51 @@
+package com.nazjara.service.jpa;
+
+import com.nazjara.model.Speciality;
+import com.nazjara.repository.SpecialityRepository;
+import com.nazjara.service.SpecialityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class SpecialityJpaService implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    @Autowired
+    public SpecialityJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialityRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialityRepository.deleteById(aLong);
+    }
+}
